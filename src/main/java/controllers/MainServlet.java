acb9468250c971e3603d768f9451e7e855dcec5e
Joey Leeuwinga <j.leeuwinga@hotmail.com>
2017-06-09 12:10:29 +0200

    Introduction of the assignment with a startpage example

---
+++ src/main/java/controllers/MainServlet.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Joey on 8-6-2017.
+ */
+@WebServlet(name = "main", urlPatterns = "/")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello, Student!");
+        // https://youtu.be/D3nnjmK1GwE?t=3m35s
+    }
+}
